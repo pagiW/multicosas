e85b44b52d6b5c5c05eb3e69767e4aad70721ad2
Juansito_the_dev <tecnosebastube@gmail.com>
2020-09-15 15:07:36 -0500

    Cambios en el js

--- code.js
+++ code.js
@@ -1,10 +1,13 @@
 var header = document.getElementById("head");
 var presi = document.getElementById("pressiona");
+var main = document.getElementById("prin");
 var state = 0;
+var ani = 0;
 var pantalla = window.matchMedia("screen and (max-width: 425px)");
 var ione = document.getElementById("n1");
 var itwo = document.getElementById("n2");
 var boton = document.getElementById("botpres");
+var animation = document.getElementById("event");
 
 pantalla.addEventListener('change', cambio);
 boton.addEventListener("click", presionasteP);
@@ -13,7 +16,10 @@ function presionasteP() {
 }
 function cambio(evento) {
     if (evento.matches == true) {
-        presi.addEventListener('click', presionar);
+        presi.addEventListener('click', presionar)
+        header.addEventListener("click", clickhover);
+        header.removeEventListener("mouseover", hover);
+        header.removeEventListener("mouseleave", nohover);
         header.innerHTML += "<p id='par'></p>";
     }
     else {
@@ -21,6 +27,9 @@ function cambio(evento) {
         header.innerHTML = '<form class="formulario" id="formulario"><input list="opciones" name="opciones" id="op"><datalist id="opciones"><option value="si"></option><option value="no"></option></datalist><button type="submit" id="boton">seleccionar</button></form>';
         header.style.backgroundColor = "green";
         state = 0;
+        header.addEventListener("mouseover", hover);
+        main.addEventListener("mouseleave", nohover);
+        header.removeEventListener("click", clickhover);
     }
 }
 cambio(pantalla);
@@ -36,5 +45,30 @@ function presionar() {
         header.style.backgroundColor = "green";
         p.innerHTML = "GREEN";
         state = 0;
+    }
+}
+function hover() {
+    animation.style.animationName = "entrar";
+    animation.style.animationDuration = "1s";
+    animation.style.animationFillMode = "forwards";
+    ani = 1;
+}
+function nohover() {
+    animation.style.animationName = "salir";
+    animation.style.animationDuration = "1s";
+    animation.style.animationFillMode = "forwards";
+    ani = 0;
+}
+function clickhover() {
+    if (ani == 0) {
+        animation.style.animationName = "entrar";
+        animation.style.animationDuration = "1s";
+        animation.style.animationFillMode = "forwards";
+        ani = 1;
+    } else {
+        animation.style.animationName = "salir";
+        animation.style.animationDuration = "1s";
+        animation.style.animationFillMode = "forwards";
+        ani = 0;
     }
 }
